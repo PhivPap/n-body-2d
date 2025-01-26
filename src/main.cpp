124a3cca174a37006a52ebedcd1abd806f025069
Phivos Papapanagiotakis <phoivos@windowslive.com>
2025-01-26 20:44:38 +0200

    Added root cmake & simple main
    CMake to build main app and link with csv-parser library.
    Main with temporary code to parse universe csv file.

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "csv.hpp"
+
+constexpr const char* INPUT_FILE_PATH = "../universe-db/universe-unit-test.csv";
+
+struct Pos2D {
+    double x, y;
+};
+
+struct Vec2D {
+    double x, y;
+};
+
+struct Body {
+    std::string id;
+    double mass;
+    Pos2D pos;
+    Vec2D vel;
+};
+
+std::vector<Body> parse_csv(std::string_view path) {
+    std::vector<Body> bodies;
+    csv::CSVReader reader(path);
+    for (csv::CSVRow& row: reader) {
+        Body body;
+        body.id = row[0].get<>();
+        body.mass = std::stod(row[1].get<>());
+        body.pos.x = std::stod(row[2].get<>());
+        body.pos.y = std::stod(row[3].get<>());
+        body.vel.x = std::stod(row[4].get<>());
+        body.vel.y = std::stod(row[5].get<>());
+        bodies.push_back(body);
+    }
+    return bodies;
+}
+
+int main() {
+    const std::vector<Body> bodies = parse_csv(INPUT_FILE_PATH);
+}
