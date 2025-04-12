19239801f07574f85853cdc081ff224f24f03e4e
Phivos Papapanagiotakis <phoivos@windowslive.com>
2025-04-12 14:03:27 +0300

    No log mangling due to multi-threading

--- src/common_lib/Logger/include/Logger/Logger.hpp
+++ src/common_lib/Logger/include/Logger/Logger.hpp
@@ -14,35 +14,27 @@ namespace Log {
 
     template <typename ...T>
     inline void error(fmt::format_string<T...> fmt_str, T &&...args) {
-        fmt::print(fg(fmt::color::crimson), "[Error] ");
-        fmt::print(fg(fmt::color::crimson), fmt_str, std::forward<T>(args)...);
-        fmt::print(fg(fmt::color::crimson), "\n");
+        fmt::print(fg(fmt::color::crimson), "[Error] {}\n", fmt::format(fmt_str, std::forward<T>(args)...));
     }
 
     template <typename ...T>
     inline void warning(fmt::format_string<T...> fmt_str, T &&...args) {
         if (verbosity >= Verbosity::WARNING) {
-            fmt::print(fg(fmt::color::orange), "[Warning] ");
-            fmt::print(fg(fmt::color::orange), fmt_str, std::forward<T>(args)...);
-            fmt::print(fg(fmt::color::orange), "\n");
+            fmt::print(fg(fmt::color::orange), "[Warning] {}\n", fmt::format(fmt_str, std::forward<T>(args)...));
         }
     }
 
     template <typename ...T>
     inline void info(fmt::format_string<T...> fmt_str, T &&...args) {
         if (verbosity >= Verbosity::INFO) {
-            fmt::print("[Info] ");
-            fmt::print(fmt_str, std::forward<T>(args)...);
-            fmt::print("\n");
+            fmt::print("[Info] {}\n", fmt::format(fmt_str, std::forward<T>(args)...));
         }
     }   
 
     template <typename ...T>
     inline void debug(fmt::format_string<T...> fmt_str, T &&...args) {
         if (verbosity >= Verbosity::DEBUG) {
-            fmt::print(fg(fmt::color::dark_cyan), "[Debug] ");
-            fmt::print(fg(fmt::color::dark_cyan), fmt_str, std::forward<T>(args)...);
-            fmt::print(fg(fmt::color::dark_cyan), "\n");
+            fmt::print(fg(fmt::color::dark_cyan), "[Debug] {}\n", fmt::format(fmt_str, std::forward<T>(args)...));
         }
     }
-}
+}   
